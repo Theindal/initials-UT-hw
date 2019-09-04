2100f3a6f8b4a688cb8fa4e145caf90cf52449c9
Theindal <50056281+Theindal@users.noreply.github.com>
2019-09-04 18:46:57 -0500

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,30 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>
+#  File: Initials.py
+#  Description: HW1
+#  Student's Name: Kosi Browne
+#  Student's UT EID: nab2768
+#  Course Name: CS 303E 
+#  Unique Number: 51200
+#
+#  Date Created: 02/09/19
+#  Date Last Modified: 04/09/19
+
+def main():
+
+	print()
+	print("   NN        NN            AAAA            BBBBBBBBBBBB        ")
+	print("   NNNN      NN            AAAA            BB        BB        ")
+	print("   NN NN     NN           AA  AA           BB        BB        ")
+	print("   NN NN     NN           AA  AA           BB        BB        ")
+	print("   NN NN     NN          AAAAAAAA          BB        BB        ")
+	print("   NN  NN    NN          AAAAAAAA          BB  BBBBBB          ")
+	print("   NN  NN    NN         AA      AA         BB        BB        ")
+	print("   NN   NN   NN         AA      AA         BB        BB        ")
+	print("   NN    NN  NN   ..   AA        AA   ..   BB        BB   ..   ")
+	print("   NN     NNNNN   ..   AA        AA   ..   BBBBBBBBBB     ..   ")
+	print()
+
+	
+>>> main()
